2eed2f50b314b524d9330b04c6292e283bf51cde
邓超 <zhongchuxiong@404.local>
2015-11-14 10:49:35 +0800

    Initial Commit

---
+++ 美术馆/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  美术馆
+//
+//  Created by 邓超 on 15/11/14.
+//  Copyright © 2015年 邓超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
